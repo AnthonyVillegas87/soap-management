f51229073e14a20fdf60512efab4da2f80f1bd79
Anthony Villegas <villegasanthony87@yahoo.com>
2024-02-05 16:09:15 -0600

    pulled my README file from repository

---
+++ src/main/java/com/encypher/spring/soapmanagement/SoapManagementApplication.java
@@ -0,0 +1,13 @@
+package com.encypher.spring.soapmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapManagementApplication.class, args);
+    }
+
+}
