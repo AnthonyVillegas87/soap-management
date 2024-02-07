f032eac01d578628f3de049b8e426ea53908086d
Anthony Villegas <villegasanthony87@yahoo.com>
2024-02-07 11:18:12 -0600

    Implement SOAP Web Service for ALL courses

--- src/main/java/com/encypher/spring/soapmanagement/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/encypher/spring/soapmanagement/soap/CourseDetailsEndpoint.java
@@ -10,35 +10,66 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import java.util.List;
+
 @Endpoint
 public class CourseDetailsEndpoint {
 
     @Autowired
     CourseDetailsService service;
+
+
     // method
     // input - request object (GetCourseDetailsRequest)
     // output - response object (GetCourseDetailsResponse)
 
 
-    // Create a method which is accepting get course details request as the input
 
+
+    // Create a method which is accepting get course details request as the input
     // Configured to accept requests of our namespace to convert from XML to Java
     @PayloadRoot(namespace = "http://www.example.org/courses", localPart = "GetCourseDetailsRequest")
     @ResponsePayload
         // The output is the get course details response
     public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
 
-        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
-        CourseDetails courseDetails = new CourseDetails();
-        courseDetails.setId(request.getId());
-        courseDetails.setName("Microservices Course");
-        courseDetails.setDescription("independently deployable services that communicate w/ each other through well defined API's");
+        Course course = service.findById(request.getId());
 
+        return getResponse(course);
+    }
+
+
+
+    @PayloadRoot(namespace = "http://www.example.org/courses", localPart = "GetAllCourseDetailsRequest")
+    @ResponsePayload
+    // The output is the get course details response
+    public GetAllCourseDetailsResponse processAllCourseDetailsRequest(@RequestPayload GetAllCourseDetailsRequest request) {
+
+       List<Course> courses = service.findAll();
+
+        return getResponse(courses);
+    }
+
+
+
+
+
+    // This method is responsible for creating details, setting the values, & returning it back ^^
+    private GetCourseDetailsResponse getResponse(Course course) {
+        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+
+        CourseDetails courseDetails = new CourseDetails();
+
+
+        courseDetails.setId(course.getId());
+
+        courseDetails.setName(course.getName());
+
+        courseDetails.setDescription(course.getDescription());
 
         response.setCourseDetails(courseDetails);
         return response;
     }
-
 
 
 }
