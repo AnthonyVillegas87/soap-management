b892efada574723364b09de5de826d74267ffd54
Anthony Villegas <villegasanthony87@yahoo.com>
2024-02-07 12:42:06 -0600

    generate Delete Service via resources

--- src/main/java/com/encypher/spring/soapmanagement/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/encypher/spring/soapmanagement/soap/CourseDetailsEndpoint.java
@@ -1,9 +1,7 @@
 package com.encypher.spring.soapmanagement.soap;
 
 import com.encypher.spring.soapmanagement.soap.service.CourseDetailsService;
-import org.example.courses.CourseDetails;
-import org.example.courses.GetCourseDetailsRequest;
-import org.example.courses.GetCourseDetailsResponse;
+import org.example.courses.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -19,7 +17,7 @@ public class CourseDetailsEndpoint {
     CourseDetailsService service;
 
 
-    // method
+    // Objective:
     // input - request object (GetCourseDetailsRequest)
     // output - response object (GetCourseDetailsResponse)
 
@@ -30,46 +28,58 @@ public class CourseDetailsEndpoint {
     // Configured to accept requests of our namespace to convert from XML to Java
     @PayloadRoot(namespace = "http://www.example.org/courses", localPart = "GetCourseDetailsRequest")
     @ResponsePayload
-        // The output is the get course details response
+        // The output is the GetCourseDetailsRequest
     public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
-
         Course course = service.findById(request.getId());
-
-        return getResponse(course);
+        return getCourseDetailsResponse(course);
     }
 
-
+    private GetCourseDetailsResponse getCourseDetailsResponse(Course course) {
+        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+        response.setCourseDetails(getDetails(course));
+        return response;
+    }
 
     @PayloadRoot(namespace = "http://www.example.org/courses", localPart = "GetAllCourseDetailsRequest")
     @ResponsePayload
-    // The output is the get course details response
+    // The output is the get ALL course details response
     public GetAllCourseDetailsResponse processAllCourseDetailsRequest(@RequestPayload GetAllCourseDetailsRequest request) {
+        List<Course> courses = service.findAll();
+        return getAllCourseDetails(courses);
+    }
 
-       List<Course> courses = service.findAll();
 
-        return getResponse(courses);
+    private GetAllCourseDetailsResponse getAllCourseDetails(List<Course> courses) {
+        GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+        for (Course course : courses) {
+            CourseDetails getCourse = getDetails(course);
+            response.getCourseDetails().add(getCourse);
+        }
+
+        return response;
     }
 
 
 
 
-
-    // This method is responsible for creating details, setting the values, & returning it back ^^
-    private GetCourseDetailsResponse getResponse(Course course) {
-        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
-
+    private CourseDetails getDetails(Course course) {
         CourseDetails courseDetails = new CourseDetails();
-
 
         courseDetails.setId(course.getId());
 
         courseDetails.setName(course.getName());
 
         courseDetails.setDescription(course.getDescription());
-
-        response.setCourseDetails(courseDetails);
-        return response;
+        return courseDetails;
     }
+
+
+
+
+
+
+
+
 
 
 }
